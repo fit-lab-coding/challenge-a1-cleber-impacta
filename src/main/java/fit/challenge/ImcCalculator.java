2fffdbb1d49c8ffc26f8e4726180eb851994f484
Cleber dos Santos Vieira <cleber.vieira@bradesco.com.br>
2021-02-03 20:12:23 -0300

    ajustes

--- src/main/java/fit/challenge/ImcCalculator.java
+++ src/main/java/fit/challenge/ImcCalculator.java
@@ -4,15 +4,15 @@ public class ImcCalculator {
 
 	public static String calculate(int peso, double altura) {
 		String retorno = "";
-		double imc = (peso)/(altura*altura);
+		double imc = (peso)/Math.pow(altura, 2);
 		
 		if(imc < 18.5) {
 			retorno = "Magreza";
-		}else if(imc >= 18.5 && imc < 25) {
+		}else if(imc < 25) {
 			retorno = "Normal";
-		}else if(imc >= 25 && imc < 30) {
+		}else if(imc < 30) {
 			retorno = "Sobrepeso";
-		}else if (imc >= 30 && imc < 40) {
+		}else if (imc < 40) {
 			retorno = "Obesidade";
 		}else {
 			retorno = "Obesidade Gravde";
@@ -23,15 +23,15 @@ public class ImcCalculator {
 
 	public static String calculate(double peso, double altura) {
 		String retorno = "";
-		double imc = (peso)/(altura*altura);
+		double imc = (peso)/Math.pow(altura, 2);
 		
 		if(imc < 18.5) {
 			retorno = "Magreza";
-		}else if(imc >= 18.5 && imc < 25) {
+		}else if(imc < 25) {
 			retorno = "Normal";
-		}else if(imc >= 25 && imc < 30) {
+		}else if(imc < 30) {
 			retorno = "Sobrepeso";
-		}else if (imc >= 30 && imc < 40) {
+		}else if (imc < 40) {
 			retorno = "Obesidade";
 		}else {
 			retorno = "Obesidade Gravde";
